45621674fe2dee0995f7a3bb0ebdfa3979de0766
Kuldeep Ghate <kuldeepghate@Kuldeeps-MacBook-Pro.local>
2018-04-20 19:05:10 -0700

    adding persistence logic

---
+++ resources/js/script.js
@@ -0,0 +1,7 @@
+
+function writeCookie(form){
+        document.cookie = "email=" + form.email.value;
+        document.cookie = "fname=" + form.fname.value;
+        document.cookie = "lname=" + form.fname.value;
+       
+}
