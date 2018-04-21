8f84ce1ed2d765ca8a6fd5e57108636bcdb98a68
Kuldeep Ghate <kuldeepghate@Kuldeeps-MacBook-Pro.local>
2018-04-20 19:47:05 -0700

    added form validations

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,9 @@
 
 function writeCookie(form){
-        document.cookie = "email=" + form.email.value;
-        document.cookie = "fname=" + form.fname.value;
-        document.cookie = "lname=" + form.fname.value;
+
+    validateForm();
+    document.cookie = "email=" + form.email.value;
+    document.cookie = "fname=" + form.fname.value;
+    document.cookie = "lname=" + form.fname.value;
        
 }
